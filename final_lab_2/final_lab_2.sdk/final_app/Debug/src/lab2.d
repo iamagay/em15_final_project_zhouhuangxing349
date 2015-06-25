854398cd92440e9eee1d7d9d77ed228985a96f54
iamagay <836707412@qq.com>
2015-06-25 14:24:11 +0800

    final project
    final project

---
+++ final_lab_2/final_lab_2.sdk/final_app/Debug/src/lab2.d
@@ -0,0 +1,46 @@
+src/lab2.o src/lab2.d: ../src/lab2.c \
+ ../../final_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../final_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../final_bsp/ps7_cortexa9_0/include/xgpio.h \
+ ../../final_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../final_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../final_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../final_bsp/ps7_cortexa9_0/include/xgpio_l.h \
+ ../../final_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../final_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../final_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../final_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../final_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../final_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../final_bsp/ps7_cortexa9_0/include/xutil.h \
+ ../../final_bsp/ps7_cortexa9_0/include/xbasic_types.h
+
+../../final_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../final_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../final_bsp/ps7_cortexa9_0/include/xgpio.h:
+
+../../final_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../final_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../final_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../final_bsp/ps7_cortexa9_0/include/xgpio_l.h:
+
+../../final_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../final_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../final_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../final_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../final_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../final_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../final_bsp/ps7_cortexa9_0/include/xutil.h:
+
+../../final_bsp/ps7_cortexa9_0/include/xbasic_types.h:
